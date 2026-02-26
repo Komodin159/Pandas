c34a72512b6b175d982e69608185f24cab9cd87b
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-02-26 12:04:20 -0500

    final

--- Nota.py
+++ Nota.py
@@ -7,35 +7,6 @@ estudiante = [
 print("--- Datos cargados ---")
 print(f"Total de estudiantes: {len(estudiante)}")
 
-<<<<<<< HEAD
-def calcular_promedio(listaNotas):
-    if len(listaNotas) == 0:
-        return 0
-
-    suma = 0
-    for nota in listaNotas:
-        suma += nota
-
-    promedio = suma / len(listaNotas)
-    return promedio
-
-print("Promedio Andres", calcular_promedio(estudiante[1]["Notas"]))
-=======
-def reporte(estudiantes):
-    print("--- Reporte Final ---")
-    for est in estudiantes:
-        suma = sum(est["Notas"])
-        promedio = suma / len(est["Notas"])
-
-        if promedio >= 3.0:
-            estado = "Aprobado"
-        else:
-            estado = "Reprobado"
-
-        print(f"Estudiante: {est["Nombre"]} su estado es: {estado}")
-
-reporte(estudiante)
-
 def calcular_promedio(listaNotas):
     if len(listaNotas) == 0:
         return 0
