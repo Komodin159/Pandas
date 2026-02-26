837886cf8bfd3873caaa20ce91534bab28f8db9a
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-02-26 11:43:29 -0500

    Cree funcion de reporte

--- Nota.py
+++ Nota.py
@@ -1,8 +1,23 @@
 estudiante = [
     {"Nombre": "Andres", "Notas": [3.0, 4.5, 4.0]},
     {"Nombre": "Maria", "Notas": [4.0, 5.0, 3.5]},
-    {"Nombre": "Fabio", "Notas": [3.6, 4.2, 3.5]},
+    {"Nombre": "Fabio", "Notas": [3.6, 2.2, 2.5]},
 ]
 
 print("--- Datos cargados ---")
 print(f"Total de estudiantes: {len(estudiante)}")
+
+def reporte(estudiantes):
+    print("--- Reporte Final ---")
+    for est in estudiantes:
+        suma = sum(est["Notas"])
+        promedio = suma / len(est["Notas"])
+
+        if promedio >= 3.0:
+            estado = "Aprobado"
+        else:
+            estado = "Reprobado"
+
+        print(f"Estudiante: {est["Nombre"]} su estado es: {estado}")
+
+reporte(estudiante)
