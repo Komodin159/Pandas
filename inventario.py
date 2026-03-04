d01dbc5125e6afc3e179666b7e96a1db5734e958
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-03-04 09:50:45 -0500

    Funcion Menu

--- inventario.py
+++ inventario.py
@@ -1,5 +1,13 @@
 def main():
 
+def menu():
+    print("INICIO DE PROGRAMA")
+    print("----GESTIONAR INVENTARIO----")
+    print("1. Agregue el producto")
+    print("2. Ver inventario completo")
+    print("3. Buscar producto")
+    print("4. Salir")
+
 
 if __name__ == "__main__":
     main()
