4273ba2387a7cac0abb1bf685ea9ba7690ebab52
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-03-04 11:20:56 -0500

    Funciones opcion del 2 al 4 creadas

--- inventario.py
+++ inventario.py
@@ -23,6 +23,31 @@ def main():
             except ValueError:
                 print("El producto y precio no son validos")
 
+        elif opcion == "2":
+            print("----DICIONARIO----")
+
+            if len(inventario) == 0:
+                print("El inventario esta vacio")
+            else:
+                for item in inventario:
+                    print(f"Producto: {item["Nombre"]}, el precio es de [{item["Precio"]}] y la cantidad existente es de [{item["Cantidad"]}]")
+        elif opcion == "3":
+            busqueda = input("Ingrese el nombre a buscar:\n").lower()
+            encontrar = False
+
+            for item in inventario:
+                if item ["Nombre"].lower() == busqueda:
+                    print(f"ENCONTRADO: el precio $ {item["Precio"]} y la cantidad de [{item["Cantidad"]}]")
+                    encontrar = True
+                    break
+
+            if not encontrar:
+                print("El producto no existe")
+
+        elif opcion == "4":
+            print("Saliendo del programa...")
+        else:
+            print("Opcion no valida, intente de nuevo")
 
 if __name__ == "__main__":
     main()
