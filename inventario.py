02acb29683c324fc2f342354e38f97d4623c63d8
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-03-04 10:45:11 -0500

    Creacion de logica opcion 1

--- inventario.py
+++ inventario.py
@@ -1,4 +1,27 @@
 def main():
+    inventario = []
+
+    while True:
+        opcion = input("Ingrese su opcion:\n")
+        if opcion == "1":
+            print("----NUEVO PRODUCTO----")
+            nombre = input("Nombre de producto:\n")
+
+            try:
+                precio = float(input("Ingrese precio: "))
+                stock = int(input("Ingrese cantidad: "))
+
+                producto = {
+                    "Nombre": nombre,
+                    "Precio": precio,
+                    "Cantidad": stock
+                }
+                inventario.append(producto)
+
+                print("El producto se guardo correctamente")
+
+            except ValueError:
+                print("El producto y precio no son validos")
 
 
 if __name__ == "__main__":
