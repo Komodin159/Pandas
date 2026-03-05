54458c8876e12e93b02c317f8b398bc3e71ac3b0
Juan David Rodriguez <juandavid.rodriguez112304@gmail.com>
2026-03-05 09:58:23 -0500

    Creamos funcion de registro empleados y dias trabajados

--- nomina.py
+++ nomina.py
@@ -3,6 +3,29 @@ import os
 def limpiar_pantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def registrar_empleado(lista_empleados):
+    print("---Registro de Empleados---")
+
+    nombre = input("Ingrese su nombre:\n")
+
+    try:
+        salario_base = float(input("Ingrese su salario mensual:"))
+        dias_trabajados = int(input("Ingrese su dias trabajados(1-30):"))
+
+        if dias_trabajados < 1 or dias_trabajados > 30:
+            print("Cantidad de dias ingresados no validos")
+            return
+
+        empleado = {
+        "Nombre":nombre,
+        "Salario":salario_base,
+        "Dias":dias_trabajados
+        }
+        lista_empleados.append(empleado)
+        print("Empleado %s registrado con exito" % (nombre))
+
+    except ValueError:
+        print("Error, ingrese un numero valido para el salario y los dias")
 
 
 if __name__ == "__main__":
